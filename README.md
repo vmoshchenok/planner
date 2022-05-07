c3edc40ba1ae586cdb25c869dabfa47b8b319194
Anna Moshchenok <Anna_Moshchenok@neimanmarcus.com>
2022-05-07 10:22:15 +0200

    added allure reporting

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Anna Moshchenok
 Need to have JAVA installed in you system non lower that version 8 (if you use macOS run $ brew install --cask java8)
 Need to have Maven installed (for MacOS run $ brew install maven)
 Need to be [IntelliJ IDEA](https://www.jetbrains.com/idea/) installed
+To generate allure report - need to insatll allure (For Mas OS brew install allure) https://docs.qameta.io/allure/#_installing_a_commandline
 
 # Run tests
 run the command $ mvn surefire-report:report in the root of the project
@@ -16,6 +17,12 @@ OR
 Open emailable report target/surefire-reports/emailable-report.html
 
 Screenshots of failed tests will be here build/reports/tests
+
+#Run tests with allure report
+run tests: mvn clean test
+generate report: allure serve target/allure-results
+
+
 
 
 
--- src/main/java/po/ToDoPage.java
+++ src/main/java/po/ToDoPage.java
@@ -2,12 +2,14 @@ package po;
 import java.util.List;
 
 import com.codeborne.selenide.CollectionCondition;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ToDoPage {
 
+    @Step("Verify the ToDo list page layout")
     public void checkPageLayout() {
         $x("//h1").shouldHave(text("todos"));
         $x("//*[@class = 'info']").shouldHave(text("Double-click to edit a todo\n" +
@@ -20,54 +22,66 @@ public class ToDoPage {
         $x("//*[contains(@class, 'new-todo')]").shouldHave(attribute("placeholder", "What needs to be done?"));
     }
 
+    @Step("Create a new ToDo item")
     public void createToDoItem(String value) {
         $x("//*[contains(@class, 'new-todo')]").val(value);
         $x("//*[contains(@class, 'new-todo')]").pressEnter();
     }
 
+    @Step("Verify that the item was added to the list")
     public void checkIfToDoItemAdded(String value) {
         $x("//label[text() = '" + value + "']").should(exist);
     }
 
+    @Step("Edit the item")
     public void editItem(String oldValue, String newValue) {
         $x("//label[text() = '" + oldValue + "']").doubleClick();
         $x("//*[@class = 'edit']").setValue(newValue);
     }
 
+    @Step("Complete a Todo item")
     public void competeToDoItem(String value) {
         $x("//label[text() = \"" + value + "\"]//preceding-sibling::input").click();
     }
 
+    @Step("Delete a Todo item")
     public void deleteToDoItem(String value) {
         $x("//label[text() = \"" + value + "\"]").click();
         $x("//label[text() = \"" + value + "\"]//following-sibling::button").click();
     }
 
+    @Step("Verify that the Todo item was completed")
     public void checkIfToDoItemCompleted(String value) {
         $x("//*[@class = 'completed']//label[text() = \"" + value + "\"]").should(visible);
     }
 
+    @Step("Verify that the Todo item was deleted")
     public void checkIfToDoItemDeleted(String value) {
         $$x("//label[text() = \"" + value + "\"]").shouldHave(CollectionCondition.size(0));
     }
 
+    @Step("Check the count of items")
     public void checkCountOfLeftItems(String count) {
         $x("//strong").shouldHave(text(count));
         $x("//*[@class = 'todo-count']").shouldHave(text(count + " items left"));
     }
 
+    @Step("Check that the ToDo  list is empty ")
     public void checkIfNoItemsPresent(){
         $x("//*[@class = 'todo-count']").should(not(exist));
     }
 
+    @Step("Clear completed items")
     public void clearCompletedItems() {
         $x("//*[@class = 'clear-completed']").click();
     }
 
+    @Step("Verify that completed items was removed")
     public void checkIfCompletedItemsRemoved() {
         $$x("//*[@class = 'clear-complete']").shouldHave(CollectionCondition.size(0));
     }
 
+    @Step("Create a few items")
     public void createFewItems(List<String> items) {
         items.forEach(item -> {
             createToDoItem(item);
